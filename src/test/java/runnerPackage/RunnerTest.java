621d19d25566c355d1231560e0dc255153915a03
Mridul Das <mridul.das72@gmail.com>
2020-06-06 20:39:54 +0100

    Changing the report location

--- src/test/java/runnerPackage/RunnerTest.java
+++ src/test/java/runnerPackage/RunnerTest.java
@@ -14,7 +14,7 @@ import java.io.File;
 @CucumberOptions(
         features = "src/test/java/Features",
         glue= {"stepDefinitions"},
-        tags={"@IAM-Bulk"},plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"},
+        tags={"@IAM-Bulk"},plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"},
         //dryRun =true,
         //plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/myreport.html"},
         //plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
