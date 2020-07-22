a2938c62cd5f5330d929531736d48eda57f08a73
mridul.das <mridul.das72@gmail.com>
2020-07-22 17:57:05 +0100

    Added extent report

---
+++ src/test/java/ExtentReports/ExtentReporting.java
@@ -0,0 +1,35 @@
+package ExtentReports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ExtentReporting {
+
+    ExtentReports extent= new ExtentReports();;
+
+
+@BeforeTest
+    public void config()
+{
+    String path=System.getProperty("user.dir")+"\\ExtRep\\index.html";
+    System.out.println(path);
+    ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+    reporter.config().setReportName("Practice Report");
+    reporter.config().setDocumentTitle("Test results");
+
+   // extent = new ExtentReports();
+    extent.attachReporter(reporter);
+    extent.setSystemInfo("tester","mridul.das");
+}
+
+@Test
+    public void intitialDemo()
+    {
+        extent.createTest("Initial Demo");
+        Assert.assertTrue(true);
+        extent.flush();
+    }
+}
