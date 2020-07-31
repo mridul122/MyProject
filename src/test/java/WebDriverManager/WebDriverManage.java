9eb23b3c117df3e736fd3fb422f4e5e6ab930b89
mridul.das <mridul.das72@gmail.com>
2020-07-31 23:56:21 +0100

    Adding webdriver listeners

--- src/test/java/WebDriverManager/WebDriverManage.java
+++ src/test/java/WebDriverManager/WebDriverManage.java
@@ -1,22 +1,41 @@
 package WebDriverManager;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 public class WebDriverManage {
 
+    EventCapture eventListener;
+    EventFiringWebDriver driver;
+    WebDriver dr;
+
 
     @Test
-    public void runBrowser()
+    public void runBrowser() throws Exception
     {
 
-       WebDriverManager.firefoxdriver().setup();
+       WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new FirefoxDriver();
+       // WebDriver driver = new FirefoxDriver();
+        dr =new ChromeDriver();
+
+        driver = new EventFiringWebDriver(dr);
+        eventListener = new EventCapture();
+        driver.register(eventListener);
+//        driver.navigate().to("https://www.google.com");
         driver.get("https://www.google.com");
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium");
+
+//        System.out.println(dr.getTitle());
+//        System.out.println(driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).getText());
+        driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
         driver.quit();
 
     }
